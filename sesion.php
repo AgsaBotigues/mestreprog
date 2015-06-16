f2d0b9508fae7ac121d46d6709ae3459b988245a
Alberto Casanova Higes <alberto.casanova@bonarea.com>
2015-06-16 09:22:20 +0200

    error redireccio

--- sesion.php
+++ sesion.php
@@ -3,7 +3,7 @@ if( !(isset($_SESSION['user']) && $_SESSION['user'] != '') ||
 	!(isset($_SESSION['password']) && $_SESSION['password'] != '') ){
 		$host = $_SERVER['HTTP_HOST'];
 		$uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-		$extra = 'index.php?error='.$error;
+		$extra = 'index.html?error='.$error;
 		header("Location: http://$host$uri/$extra");
 }
 ?>
