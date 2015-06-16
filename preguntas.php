f6e8dd1ddf325918f4d7d572891c8dcd3b58aac7
Alberto Casanova Higes <alberto.casanova@bonarea.com>
2015-06-16 09:39:24 +0200

    Redireccio fitxers

--- preguntas.php
+++ preguntas.php
@@ -7,7 +7,7 @@ $error = $conexio->conectar();
 if( $error != 0 ){
 	$host = $_SERVER['HTTP_HOST'];
 	$uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-	$extra = 'index.php?error='.$error;
+	$extra = 'index.html?error='.$error;
 	header("Location: http://$host$uri/$extra");
 }else{
 	$codigo = $_POST['codigo'];
